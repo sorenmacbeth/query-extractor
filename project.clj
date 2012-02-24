92f7d26086a06bb7c41c93874008bdf933d05582
Soren Macbeth <soren@dopeness.org>
2012-02-24 13:27:44 -0800

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject query-extractor "0.0.7"
+(defproject query-extractor "0.0.8"
   :description "A library for extracting search query strings for HTTP referrer strings"
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :dev-dependencies [[lein-midje "1.0.7"]
