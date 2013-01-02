2ce1171896a9be2a1daf31c01cc9c9d5c413737d
Soren Macbeth <soren@dopeness.org>
2013-01-01 16:38:48 -0800

    bump to lein 2, clojure 1.4

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject query-extractor "0.0.8"
+(defproject query-extractor "0.0.9"
   :description "A library for extracting search query strings for HTTP referrer strings"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
-  :dev-dependencies [[lein-midje "1.0.7"]
-                     [midje "1.3.2-SNAPSHOT"]])
-
+  :url "http://github.com/sorenmacbeth/query-extractor"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :profiles {:dev
+             {:dependencies [[midje "1.4.0"]]
+              :plugins [[lein-midje "2.0.3"]]}})
