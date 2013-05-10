a8e34dc905f4a3ca950b70398a5ea4facea56cff
Soren Macbeth <soren@dopeness.org>
2013-05-10 14:41:28 -0700

    bump midje

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :profiles {:dev
-             {:dependencies [[midje "1.5.0"]]
-              :plugins [[lein-midje "3.0.0"]]}})
+             {:dependencies [[midje "1.5.1"]]
+              :plugins [[lein-midje "3.0.1"]]}})
